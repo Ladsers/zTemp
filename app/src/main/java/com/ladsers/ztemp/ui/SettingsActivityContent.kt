526a9bf035fceaf27e5c5433eac8c69a54a8937f
Max Korolev <37272192+Ladsers@users.noreply.github.com>
2023-12-07 20:24:31 +0300

    Implemented scroll indicator.

--- app/src/main/java/com/ladsers/ztemp/ui/SettingsActivityContent.kt
+++ app/src/main/java/com/ladsers/ztemp/ui/SettingsActivityContent.kt
@@ -1,15 +1,9 @@
 package com.ladsers.ztemp.ui
 
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalSavedStateRegistryOwner
-import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.wear.compose.material.Scaffold
-import androidx.wear.compose.material.TimeText
-import androidx.wear.compose.material.TimeTextDefaults
 import com.ladsers.ztemp.domain.ZtempApplication
 import com.ladsers.ztemp.domain.viewModels.SettingsViewModel
 import com.ladsers.ztemp.ui.screens.SettingsScreen
@@ -29,16 +23,5 @@ fun SettingsActivityContent(
         )
     )
 
-    Scaffold(
-        modifier = Modifier.fillMaxWidth(),
-        timeText = {
-            TimeText(
-                timeTextStyle = TimeTextDefaults.timeTextStyle(
-                    fontSize = 12.sp
-                )
-            )
-        }
-    ) {
-        SettingsScreen(settingsViewModel, updateAvailable, deviceName, goToWebsite)
-    }
+    SettingsScreen(settingsViewModel, updateAvailable, deviceName, goToWebsite)
 }
--- app/src/main/java/com/ladsers/ztemp/ui/screens/SettingsScreen.kt
+++ app/src/main/java/com/ladsers/ztemp/ui/screens/SettingsScreen.kt
@@ -1,24 +1,41 @@
 package com.ladsers.ztemp.ui.screens
 
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.focusable
+import androidx.compose.foundation.gestures.animateScrollBy
+import androidx.compose.foundation.gestures.scrollBy
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.rotary.onRotaryScrollEvent
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.sp
 import androidx.wear.compose.material.CardDefaults
 import androidx.wear.compose.material.ListHeader
 import androidx.wear.compose.material.MaterialTheme
+import androidx.wear.compose.material.PositionIndicator
+import androidx.wear.compose.material.Scaffold
 import androidx.wear.compose.material.ScalingLazyColumn
 import androidx.wear.compose.material.Text
+import androidx.wear.compose.material.TimeText
+import androidx.wear.compose.material.TimeTextDefaults
 import androidx.wear.compose.material.TitleCard
+import androidx.wear.compose.material.Vignette
+import androidx.wear.compose.material.VignettePosition
+import androidx.wear.compose.material.rememberScalingLazyListState
 import com.ladsers.ztemp.BuildConfig
 import com.ladsers.ztemp.R
 import com.ladsers.ztemp.domain.viewModels.SettingsViewModel
 import com.ladsers.ztemp.ui.theme.DarkOrange
-import com.ladsers.ztemp.ui.theme.Orange
 import com.ladsers.ztemp.ui.theme.Teal
+import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun SettingsScreen(
     viewModel: SettingsViewModel,
@@ -26,79 +43,121 @@ fun SettingsScreen(
     deviceName: String,
     goToWebsite: (String) -> Unit
 ) {
-    ScalingLazyColumn(
-        modifier = Modifier.fillMaxWidth()
+
+    val listState = rememberScalingLazyListState()
+
+    Scaffold(
+        timeText = {
+            TimeText(
+                timeTextStyle = TimeTextDefaults.timeTextStyle(
+                    fontSize = 12.sp
+                )
+            )
+        },
+        vignette = {
+            Vignette(vignettePosition = VignettePosition.TopAndBottom)
+        },
+        positionIndicator = {
+            PositionIndicator(scalingLazyListState = listState)
+        }
     ) {
-        item {
-            ListHeader {
-                Text(text = "Настройки")
-            }
-        }
-        if (updateAvailable) {
+
+        val focusRequester = remember { FocusRequester() }
+        val coroutineScope = rememberCoroutineScope()
+
+        ScalingLazyColumn(
+            modifier = Modifier
+                .onRotaryScrollEvent {
+                    coroutineScope.launch {
+                        listState.scrollBy(it.verticalScrollPixels)
+                        listState.animateScrollBy(0f)
+                    }
+                    true
+                }
+                .focusRequester(focusRequester)
+                .focusable(),
+            state = listState
+        ) {
             item {
-                TitleCard(
-                    title = {
-                        Text(
-                            stringResource(id = R.string.updateAvailable),
-                            fontSize = 18.sp,
-                            color = Color.White
-                        )
-                    },
-                    backgroundPainter = CardDefaults.cardBackgroundPainter(
-                        startBackgroundColor = DarkOrange,
-                        endBackgroundColor = MaterialTheme.colors.surface
-                    ),
-                    onClick = { goToWebsite("https://www.ladsers.com/ztemp/download") }
-                ) {}
+                ListHeader {
+                    Text(
+                        text = stringResource(id = R.string.title_activity_settings),
+                        fontSize = 18.sp
+                    )
+                }
+            }
+            if (updateAvailable) {
+                item {
+                    TitleCard(
+                        title = {
+                            Text(
+                                stringResource(id = R.string.updateAvailable),
+                                fontSize = 18.sp,
+                                color = Color.White
+                            )
+                        },
+                        backgroundPainter = CardDefaults.cardBackgroundPainter(
+                            startBackgroundColor = DarkOrange,
+                            endBackgroundColor = MaterialTheme.colors.surface
+                        ),
+                        onClick = { goToWebsite("https://www.ladsers.com/ztemp/download") }
+                    ) {}
+                }
+            }
+            item {
+                SettingsCard(
+                    title = stringResource(id = R.string.changeDevice),
+                    content = deviceName
+                )
+            }
+            item {
+                TextInputChip(
+                    label = stringResource(id = R.string.presetTemp1),
+                    valueState = viewModel.presetTemp1,
+                    onUpdateValue = viewModel::updatePresetTemp1,
+                    enabled = viewModel.addFeatures.value
+                )
+            }
+            item {
+                TextInputChip(
+                    label = stringResource(id = R.string.presetTemp2),
+                    valueState = viewModel.presetTemp2,
+                    onUpdateValue = viewModel::updatePresetTemp2,
+                    enabled = viewModel.addFeatures.value
+                )
+            }
+            item {
+                SettingsCard(
+                    title = stringResource(id = R.string.supportDeveloper),
+                    content = stringResource(id = R.string.supportDeveloperDescription)
+                )
+            }
+            item {
+                SettingsCard(
+                    title = stringResource(id = R.string.appWebsite),
+                    enabled = true,
+                    action = { goToWebsite("https://www.ladsers.com/ztemp") })
+            }
+            item {
+                SettingsCard(title = stringResource(id = R.string.createdBy))
+            }
+            item {
+                SettingsCard(title = stringResource(id = R.string.licenseThirdParty),
+                    enabled = true,
+                    action = { goToWebsite("https://www.ladsers.com/ztemp/license") })
+            }
+            item {
+                SettingsCard(
+                    title = stringResource(id = R.string.version),
+                    content = BuildConfig.VERSION_NAME
+                )
+            }
+            item {
+                SettingsCard(title = stringResource(id = R.string.logOut))
             }
         }
-        item {
-            TextInputChip(
-                label = stringResource(id = R.string.presetTemp1),
-                valueState = viewModel.presetTemp1,
-                onUpdateValue = viewModel::updatePresetTemp1,
-                enabled = viewModel.addFeatures.value
-            )
-        }
-        item {
-            TextInputChip(
-                label = stringResource(id = R.string.presetTemp2),
-                valueState = viewModel.presetTemp2,
-                onUpdateValue = viewModel::updatePresetTemp2,
-                enabled = viewModel.addFeatures.value
-            )
-        }
-        item {
-            SettingsCard(title = stringResource(id = R.string.changeDevice), content = deviceName)
-        }
-        item {
-            SettingsCard(
-                title = stringResource(id = R.string.supportProject),
-                content = stringResource(id = R.string.supportProjectDescription)
-            )
-        }
-        item {
-            SettingsCard(
-                title = stringResource(id = R.string.appWebsite),
-                enabled = true,
-                action = { goToWebsite("https://www.ladsers.com/ztemp") })
-        }
-        item {
-            SettingsCard(title = stringResource(id = R.string.createdBy))
-        }
-        item {
-            SettingsCard(title = stringResource(id = R.string.licenseThirdParty),
-                enabled = true,
-                action = { goToWebsite("https://www.ladsers.com/ztemp/license") })
-        }
-        item {
-            SettingsCard(
-                title = stringResource(id = R.string.version),
-                content = BuildConfig.VERSION_NAME
-            )
-        }
-        item {
-            SettingsCard(title = stringResource(id = R.string.logOut))
+        LaunchedEffect(Unit) {
+            focusRequester.requestFocus()
         }
     }
 }
